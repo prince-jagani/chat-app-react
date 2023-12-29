5f3b34aed2cbc1f924986e103e86572ec2a10b0f
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2023-12-30 01:32:20 +0530

    config codux board size

--- client/src/_codux/boards/app/app.board.tsx
+++ client/src/_codux/boards/app/app.board.tsx
@@ -5,4 +5,10 @@ export default createBoard({
     name: 'App',
     Board: () => <App />,
     isSnippet: true,
+    environmentProps: {
+        canvasWidth: 1995,
+        canvasHeight: 1080,
+        windowWidth: 1920,
+        windowHeight: 1080
+    }
 });
